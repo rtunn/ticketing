10a551121d9f612b66ae4659a11a71ed6375cbc4
rtunn <rtunnell9@gmail.com>
2022-04-13 16:20:49 -0400

    remove unnecessary headers in axios client

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -4,12 +4,6 @@ const buildClient = ({ req }) => {
   if (typeof window === "undefined") {
     return axios.create({
       baseURL: "http://ticketingapp.empirewebdevelopment.com",
-      headers: {
-        "Cache-Control": "no-cache",
-        Pragma: "no-cache",
-        Expires: "0",
-        ...req.headers,
-      },
     });
   } else {
     return axios.create({
